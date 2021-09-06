bde7bcd56c9862a50d99e5eb858e55a3c1fad451
cam-emmauscollege <cam@emmauscollege.nl>
2021-09-06 11:20:40 +0000

    maandag 6e uur

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var x = 50;
+var y = 50;
+var speedX = 2;
+var speedY = 5;
+
+
+
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -7,8 +14,7 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  
 }
 
 
@@ -18,9 +24,21 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+
   // stel vulkleur in
-  fill(100, 100, 255);
+  fill(255, 255, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x, y, 80, 80);
+
+  // positie van bal updaten
+  x = x + speedX;
+  y = y + speedY;
+
+  if (y === 720) {
+    speedY = speedY * -1;
+  }
+
 }
